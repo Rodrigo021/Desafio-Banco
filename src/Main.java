3cbc7ec4a33a751bc2812f482efc63673eca3dd6
Rodrigo <rodrigo.srj@hotmail.com>
2024-06-13 16:57:17 -0300

    Desafio Banco

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente rodrigo = new Cliente();
+		rodrigo.setNome("rodrigo");
+		
+		Conta cc = new ContaCorrente(rodrigo);
+		Conta poupanca = new ContaPoupanca(rodrigo);
+
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
